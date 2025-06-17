efca054d6d00b0a482422b475eea266ab5db7b4d
Roxanne-Hudan <roxanne.colosia@hotmail.com>
2025-06-16 23:27:48 -0600

    htmls de Formulario

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Estudiante, Profesor, Curso, Entregable
+from .forms import CursoFormulario, ProfesorFormulario
 
 # Create your views here.
 
@@ -39,3 +40,36 @@ def cursoFormulario(request):
         return render(request, "AppCoder/index.html")
     return render(request, "AppCoder/formulario/cursoFormulario.html")
 
+def cursoFormulario2(request):
+    if request.method == "POST":
+        miFormulario = CursoFormulario(request.POST)
+        print(miFormulario)
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
+            curso = Curso(nombre=informacion["curso"],camada=informacion["camada"])
+            curso.save()
+            return render(request, "AppCoder/inicio.html")
+    else:
+        miFormulario = CursoFormulario()        
+    
+    return render(request, "AppCoder/formulario/cursoFormulario2.html", {"miFormulario": miFormulario})
+
+
+def profesorFormulario(request):
+
+    if request.method == 'POST':
+        miFormulario = ProfesorFormulario(request.POST)  # aquí llega toda la información del html
+        if miFormulario.is_valid():  # Si pasó la validación de Django
+            informacion = miFormulario.cleaned_data
+            profesor = Profesor(
+                nombre=informacion['nombre'],
+                apellido=informacion['apellido'],
+                email=informacion['email'],
+                profesion=informacion['profesion']
+            )
+            profesor.save()
+            return render(request, "AppCoder/index.html")  # Vuelvo al inicio o a donde quieran
+    else:
+        miFormulario = ProfesorFormulario()  # Formulario vacío para construir el html
+
+    return render(request, "AppCoder/formulario/profesorFormulario.html", {"miFormulario": miFormulario})
